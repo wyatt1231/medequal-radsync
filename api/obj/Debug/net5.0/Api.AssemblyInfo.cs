4a0a733982dca71ea486352adbce3a5fd0dbb1df
marc <marc@silicate.com.au>
2024-12-19 03:43:43 +0800

    save apidll

--- api/obj/Debug/net5.0/Api.AssemblyInfo.cs
+++ api/obj/Debug/net5.0/Api.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a6042ff01784baf132d934c67b9edf0b247828e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58a93cbdfcc68153eb68b8a1f4bee31130e60126")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
